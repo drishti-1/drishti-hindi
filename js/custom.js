32f299b920341f188a0629d735da9cad47710ab4
drishti-1 <imagicdelhi99@gmail.com>
2024-07-29 18:50:29 +0530

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -22,4 +22,26 @@ items: 3
 }
 });
 
+$('.owl-prefects').owlCarousel({
+margin: 20,
+nav: false,
+loop: true,
+autoplay:true,
+autoplayTimeout: 3000,
+autoplayHoverPause:true,
+dots:true,
+navText:['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+responsive: {
+0: {
+items: 1
+},
+600: {
+items: 1
+},
+1000: {
+items: 3
+}
+}
+});
+
 });
